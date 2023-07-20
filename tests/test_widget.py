d203562f9b7dd8bf70b39746414b676d059d2b89
maxsenh <maximilian.senftleben@googlemail.com>
2023-07-20 13:48:18 +0200

    moved tests folder into main

---
+++ tests/test_widget.py
@@ -0,0 +1,21 @@
+
+import sys
+import os
+import pytest
+import visualizer
+
+# this is your plugin name declared in your napari.plugins entry point
+MY_PLUGIN_NAME = "m25-napari"
+# the name of your widget(s)
+MY_WIDGET_NAMES = ["M25Controls"]
+
+
+@pytest.mark.parametrize("widget_name", MY_WIDGET_NAMES)
+def test_something_with_viewer(widget_name, make_napari_viewer):
+    viewer = make_napari_viewer()
+    num_dw = len(viewer.window._dock_widgets)
+    viewer.window.add_plugin_dock_widget(
+        plugin_name=MY_PLUGIN_NAME, widget_name=widget_name
+    )
+    assert len(viewer.window._dock_widgets) == num_dw + 1
+
